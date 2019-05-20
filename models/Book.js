e45d59f2ae9ecfb9c87a90da9f05fc9d55527c86
Mary Robin Rose <maryrobinrose@gmail.com>
2019-05-20 13:42:53 -0700

    add data types

---
+++ models/Book.js
@@ -0,0 +1,40 @@
+'use strict';
+var dateFormat = require('dateformat');
+
+module.exports = function(sequelize, DataTypes) {
+  var Article = sequelize.define('Article', {
+    title: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: "Title is required"
+        }
+      }
+    },
+    author: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: "Author is required"
+        }
+      }
+    },
+    genre: DataTypes.STRING,
+    year: DataTypes.INTEGER
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    },
+    instanceMethods: {
+      publishedAt: function() {
+        return dateFormat(this.createdAt, "dddd, mmmm dS, yyyy, h:MM TT");
+      },
+      shortDescription: function(){
+        return this.body.length > 30 ? this.body.substr(0, 30) + "..." : this.body;
+      }
+    }
+  });
+  return Article;
+};
